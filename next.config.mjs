744f942ffe5c895a2bbb0341cd842f6f6f36b2e5
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2025-05-09 17:45:49 +0900

    ✨ next config cjs -> mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
@@ -29,4 +29,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withVanillaExtract(nextConfig);
+export default withVanillaExtract(nextConfig);
